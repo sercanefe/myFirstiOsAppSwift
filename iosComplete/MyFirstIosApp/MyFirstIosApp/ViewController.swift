297a264a5d9dc38728ce923a6cb7a7b8715674bb
Sercan Efe Karaman <70342545+sercanefe@users.noreply.github.com>
2021-10-14 22:12:12 +0300

    Add files via upload

---
+++ iosComplete/MyFirstIosApp/MyFirstIosApp/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MyFirstIosApp
+//
+//  Created by Sercan Efe KARAMAN on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var Duzenle: UIButton!
+    
+    @IBOutlet weak var sleepIcon: UIImageView!
+    
+    @IBOutlet weak var plusButton: UIStepper!
+    
+    @IBOutlet weak var alarmLabel: UILabel!
+    
+    @IBOutlet weak var uykuZamani: UILabel!
+    
+    @IBOutlet weak var alarmYok: UILabel!
+    
+    @IBOutlet weak var ayarla: UITextField!
+    
+    @IBOutlet weak var diger: UILabel!
+    
+    @IBOutlet weak var clock1: UILabel!
+    
+    @IBOutlet weak var alarm1: UILabel!
+    
+    @IBOutlet weak var switch1: UISwitch!
+    
+}
+
---
+++ iosComplete/Playgrounds/Variables.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//Variables & Constants
+
+//snake_case
+//camelCase
+
+var greeting = "Hello, playground"
+
+var userName = "James"
+print(userName)
+
+var userSurname = "Hetfield"
+
+userName = "Lars"
+print(userName)
+
+let userAge = 50.0
+
+let pi = 3.14
+
+userAge * pi
